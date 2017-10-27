ef4868ff32e08043fb7b87990573284049bfd2b0
waleedqk <wqkhan@uwaterloo.ca>
2017-10-27 17:54:55 -0400

    Commit

--- raspbian/cameara/take_picture.bash
+++ raspbian/cameara/take_picture.bash
@@ -6,3 +6,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_DIR="$(dirname "$SCRIPT_DIR")"
 
 raspistill -o $HOME"/Pictures/image.jpg"
+
+sudo fbi -T 2 -d /dev/fb1 -noverbose -a $HOME"/Pictures/image.jpg"
