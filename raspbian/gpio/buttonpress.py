09b628c420e34388f23026c136fb22010f9e9436
Odin <odin@raspberrypi>
2017-11-14 03:47:02 +0000

    Commit

--- raspbian/gpio/buttonpress.py
+++ raspbian/gpio/buttonpress.py
@@ -1,12 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
+bt = 5
+
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(bt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(16)
+    input_state = GPIO.input(bt)
     if input_state == False:
         print('Button Pressed')
         time.sleep(0.2)
