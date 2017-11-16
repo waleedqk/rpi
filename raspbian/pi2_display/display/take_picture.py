4b04766b3f13afd4ef504b0fb309f9c430fbc447
waleedqk <wqkhan@uwaterloo.ca>
2017-11-16 17:42:47 -0500

    Commit

--- raspbian/pi2_display/display/take_picture.py
+++ raspbian/pi2_display/display/take_picture.py
@@ -1,8 +1,12 @@
 from picamera import PiCamera
 from time import sleep
+import os
+
+
+home = os.environ['HOME']
 
 camera = PiCamera()
 camera.start_preview()
 sleep(5)
-camera.capture('/home/odin/Pictures/image.jpg')
+camera.capture(home + '/Pictures/image.jpg')
 camera.stop_preview()
