1fbcca2c3f0308395d76e2db210b07f66663fe93
wqkhan <wqkhan@uwaterloo.ca>
2017-11-15 23:47:13 -0500

    Commit

--- raspbian/camera/06_2_gpio_pic.py
+++ raspbian/camera/06_2_gpio_pic.py
@@ -2,10 +2,10 @@ from picamera import PiCamera
 from time import sleep
 from gpiozero import Button
 
-  button = Button(16)
-  camera = PiCamera()
+button = Button(16)
+camera = PiCamera()
 
-  camera.start_preview()
-  button.wait_for_press()
-  camera.capture('~/Pictures/image.jpg')
-  camera.stop_preview()
+camera.start_preview()
+button.wait_for_press()
+camera.capture('~/Pictures/image.jpg')
+camera.stop_preview()
