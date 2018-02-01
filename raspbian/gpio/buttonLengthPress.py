9254550675a313008aa3de46e4d1ab45060a52e4
waleedqk <wqkhan@uwaterloo.ca>
2018-02-01 15:26:44 -0500

    Commit

--- raspbian/gpio/buttonLengthPress.py
+++ raspbian/gpio/buttonLengthPress.py
@@ -4,7 +4,7 @@ import time
 import RPi.GPIO as GPIO
 
 gpio_pin=26 # The GPIO pin the button is attached to
-longpress_threshold=5 # If button is held this length of time, tells system to leave light on
+longpress_threshold=3 # If button is held this length of time (in seconds)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
