7294ba5d72a458b3ba8cba59913878182937f34d
waleedqk <wqkhan@uwaterloo.ca>
2018-02-01 15:01:13 -0500

    Commit

--- raspbian/pizw_xmas/test_cam_button.py
+++ raspbian/pizw_xmas/test_cam_button.py
@@ -6,22 +6,37 @@ from time import sleep
 import time, os, sys
 import getpass, socket
 
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+CAM_PIN = 26
+
 # The current user and hostname of the program
 cur_user = getpass.getuser()
 hostname = socket.gethostname()
 
-# Get the current time
-getthetime = time.strftime("%Y%m%d-%H%M%S")
-
 # create an instance of the PiCamera class
 camera = picamera.PiCamera()
 
+def take_pic(*args,**kwargs):
+    # Get the current time
+    getthetime = time.strftime("%Y%m%d-%H%M%S")
 
-# take a picture
-image_name = 'image_'+str(getthetime)+'.jpg'
-camera.capture('/home/'+cur_user+'/Pictures/'+image_name)
+    # take a picture
+    image_name = 'image_'+str(getthetime)+'.jpg'
+    camera.capture('/home/'+cur_user+'/Pictures/'+image_name)
+
+    os.system('sudo fbi -T 2 -d /dev/fb1 -noverbose -a /home/'+cur_user+'/Pictures/'+image_name)
 
 
-camera.close()
 
-os.system('sudo fbi -T 2 -d /dev/fb1 -noverbose -a /home/'+cur_user+'/Pictures/'+image_name)
+if __name__ == "__main__":
+
+    while True:
+        input_state = GPIO.input(CAM_PIN)
+        if input_state == False:
+            print('Button Pressed')
+            time.sleep(0.2)
+
+    camera.close()
