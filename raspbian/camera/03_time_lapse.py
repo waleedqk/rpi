297f1580e36607743d75b309d8b92b83e4571df2
wqkhan <wqkhan@uwaterloo.ca>
2018-10-17 13:35:14 -0400

    Commit

--- raspbian/camera/03_time_lapse.py
+++ raspbian/camera/03_time_lapse.py
@@ -1,9 +1,22 @@
+import sys, os, time, datetime
 from picamera import PiCamera
 from time import sleep
 
+# path to directory of python file
+dirname, filename = os.path.split(os.path.abspath(__file__))
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(os.getcwd())
+
+basedir = os.environ["HOME"]+"/Pictures/"
+
+
 camera = PiCamera()
 camera.start_preview()
+
 for i in range(5):
     sleep(5)
-    camera.capture('/home/odin/Pictures/image%s.jpg' % i)
+    datetime_now = datetime.datetime.now().strftime("%Y_%m_%d__%H-%M-%S")
+    image = "{}image_{}_{}.jpg".format(basedir, str(i).zfill(2), datetime_now)
+    
+    camera.capture(image)
 camera.stop_preview()
