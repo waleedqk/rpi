c158abb0ffb96f9dcaaaaa309ad4a1fe5537c36a
wqkhan <wqkhan@uwaterloo.ca>
2018-10-17 13:27:41 -0400

    Commit

--- raspbian/camera/02_take_picture.py
+++ raspbian/camera/02_take_picture.py
@@ -1,8 +1,21 @@
+import sys, os, time, datetime
+
+# path to directory of python file
+dirname, filename = os.path.split(os.path.abspath(__file__))
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(os.getcwd())
+
+basedir = os.environ["HOME"]+"/Pictures/"
+
 from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
 camera.start_preview()
 sleep(5)
-camera.capture('/home/odin/Pictures/image.jpg')
+
+datetime_now = datetime.datetime.now().strftime("%y_%m_%d__%M-%H-%S")
+image = "{}image_{}.jpg".format(basedir, datetime_now)
+
+camera.capture(image)
 camera.stop_preview()
