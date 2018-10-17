7409fa45b84ba4bdaa32803cd934ba64f3769600
wqkhan <wqkhan@uwaterloo.ca>
2018-10-17 01:54:30 -0400

    Commit

--- raspbian/camera/take_picture.bash
+++ raspbian/camera/take_picture.bash
@@ -5,6 +5,7 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_DIR="$(dirname "$SCRIPT_DIR")"
 
-raspistill -o $HOME"/Pictures/image.jpg"
+now=`date '+%Y_%m_%d__%H_%M_%S'`;
+raspistill -o $HOME"/Pictures/image_${now}.jpg"
 
 sudo fbi -T 2 -d /dev/fb1 -noverbose -a $HOME"/Pictures/image.jpg"
